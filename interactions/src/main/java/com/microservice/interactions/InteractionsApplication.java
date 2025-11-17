fc35ccb79f801e352b1e25c6cd56a3fc8b99603e
Raul <ra.fernandezduocuc.cl.com>
2025-11-17 20:57:14 -0300

    Se agregan carpetas y archivos principales de cada microservicio

---
+++ interactions/src/main/java/com/microservice/interactions/InteractionsApplication.java
@@ -0,0 +1,13 @@
+package com.microservice.interactions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InteractionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InteractionsApplication.class, args);
+	}
+
+}
---
+++ interactions/src/test/java/com/microservice/interactions/InteractionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservice.interactions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InteractionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
