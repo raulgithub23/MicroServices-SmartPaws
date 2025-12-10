b86a0b70db3786ae64268f2392ce74b5c070626c
Riskoder <g.vidal1g+1@gmail.com>
2025-12-09 22:44:05 -0300

    urls de microservicios

--- pet/src/main/java/com/microservice/pet/client/UserClient.java
+++ pet/src/main/java/com/microservice/pet/client/UserClient.java
@@ -6,7 +6,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "microservice-auth", url = "http://localhost:8081") 
+@FeignClient(name = "microservice-auth", url = "https://xd6w381w-8081.use2.devtunnels.ms/") 
 public interface UserClient {
 
     @GetMapping("/auth/user/{id}")
